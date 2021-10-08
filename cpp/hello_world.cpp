3178525fd37e3464c2b543e7cf0647b01c288364
omkarkurund <72203658+omkarkurund@users.noreply.github.com>
2021-10-08 12:57:27 +0530

    update hello_world.cpp

--- cpp/hello_world.cpp
+++ cpp/hello_world.cpp
@@ -1,7 +1,7 @@
-// Run as g++ hello_world.cpp then ./a.out
+// Run as c++ hello_world.cpp then ./a.out
 #include<iostream>
 int main() {
 
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello, World!!!" << std::endl;
 	return 0;
 }
